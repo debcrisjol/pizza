be1c28989604a1d41fa1df8d64e15929cebc4c63
debcrisjol <debcrisjol@gmail.com>
2022-12-20 21:09:28 +0000

    post verif

--- templates/add.php
+++ templates/add.php
@@ -1,6 +1,31 @@
 <?php
+// protect from XSS attacks with 'htmlspecialchars'
+if(isset($_POST['submit'])){
+    // echo htmlspeacialchars($_POST['email']);
+    // echo htmlspeacialchars($_POST['title']);
+    // echo htmlspeacialchars($_POST['ingredients']);
 
+// check email
+    if(empty($_POST['email'])){
+        echo 'An email is required <br/>';
+    }else{
+        echo $_POST['email'];
+    }
+    // check title
+    if(empty($_POST['title'])){
+        echo 'An title is required <br/>';
+    }else{
+        echo $_POST['title'];
+    }
+// check ingredients
+if(empty($_POST['ingredients'])){
+    echo 'An ingredient is required <br/>';
+}else{
+    echo $_POST['ingredients'];
+}
+// end of the POST
 
+}
 ?>
 
 
@@ -12,7 +37,7 @@
 
 <section class="container text-secondary ">
     <h4 class="text-center">Add a Pizza</h4>
-    <form class="d-flex flex-column justify-content-center align-items-center p-3 fs-5" action="add.php" method="GET">
+    <form class="d-flex flex-column justify-content-center align-items-center p-3 fs-5" action="add.php" method="POST">
         <label>Your Email</label>
         <input type="text" name="email" class="w-50">
         <label>Pizza Title</label>
