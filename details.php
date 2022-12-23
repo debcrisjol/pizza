307b89e71b41cd851c33c2caa1d6044e000f6133
debcrisjol <debcrisjol@gmail.com>
2022-12-23 00:57:46 +0000

    with delete item

--- details.php
+++ details.php
@@ -1,47 +1,65 @@
-<?php
-include('config/db_connect.php');
+<?php 
 
-// check GET request id param
-if(isset($_GET['id'])){
-    
-    // escape sql chars
-    $id = mysqli_real_escape_string($conn, $_GET['id']);
+	include('config/db_connect.php');
 
-    // make sql
-    $sql = "SELECT * FROM pizzas WHERE id = $id";
+	if(isset($_POST['delete'])){
 
-    // get the query result
-    $result = mysqli_query($conn, $sql);
+		$id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
 
-    // fetch result in array format
-    $pizza = mysqli_fetch_assoc($result);
+		$sql = "DELETE FROM pizzas WHERE id = $id_to_delete";
 
-    mysqli_free_result($result);
-    mysqli_close($conn);
+		if(mysqli_query($conn, $sql)){
+			header('Location: index.php');
+		} else {
+			echo 'query error: '. mysqli_error($conn);
+		}
 
-}
+	}
 
+	// check GET request id param
+	if(isset($_GET['id'])){
+		
+		// escape sql chars
+		$id = mysqli_real_escape_string($conn, $_GET['id']);
 
+		// make sql
+		$sql = "SELECT * FROM pizzas WHERE id = $id";
 
+		// get the query result
+		$result = mysqli_query($conn, $sql);
 
+		// fetch result in array format
+		$pizza = mysqli_fetch_assoc($result);
+
+		mysqli_free_result($result);
+		mysqli_close($conn);
+
+	}
 
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
 
 <?php include('templates/header.php') ?>
-<div class="container text-center m-5"> 
+<div class="container text-center m-5">
 <?php if($pizza): ?>
 			<h4><?php echo $pizza['title']; ?></h4>
 			<p>Created by <?php echo $pizza['email']; ?></p>
 			<p><?php echo date($pizza['created_at']); ?></p>
 			<h5>Ingredients:</h5>
 			<p><?php echo $pizza['ingredients']; ?></p>
+
+			<!-- DELETE FORM -->
+			<form action="details.php" method="POST">
+				<input type="hidden" name="id_to_delete" value="<?php echo $pizza['id']; ?>">
+				<input type="submit" name="delete" value="Delete" class="btn btn-warning">
+			</form>
+
 		<?php else: ?>
 			<h5>No such pizza exists.</h5>
 		<?php endif ?>
-
 </div>
 <?php include('templates/footer.php') ?>
 
